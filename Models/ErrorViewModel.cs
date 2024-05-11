727671d25711b2412ee153f7d81543bb09ab3270
Dangvanlanh <ngantamducsmile@gmail.com>
2024-05-11 13:01:19 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ReciclingGarbage.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
